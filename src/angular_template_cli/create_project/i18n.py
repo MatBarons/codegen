1af985230b73bd61d9103081611d84fec97faa73
Matteo Barone <matteo.barone1199@gmail.com>
2025-03-03 11:52:11 +0100

    added i18n and reworked sass

--- src/angular_template_cli/create_project/i18n.py
+++ src/angular_template_cli/create_project/i18n.py
@@ -1,2 +1,9 @@
+import os
+from utils.utils import copy_folders
+
+
 def add_i18n_support(project_path, languages):
+    source_dir = os.path.join(os.getcwd(),"create_project","data","i18n")
+    destination_dir = os.path.join(project_path,"src","assets")
+    copy_folders(source_dir,destination_dir)
     return 
